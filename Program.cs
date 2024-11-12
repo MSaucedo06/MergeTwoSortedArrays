a7b5ae3b1f64d0ecee6051e7fc0f289fb11bf87b
MSaucedo06 <miguel.saucedo06@gmail.com>
2024-11-11 22:28:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = [1,7,9];
+            int[] B = [2, 4, 8, 0, 0, 0];
+
+            MergeArray(A, B);
+            Console.ReadLine();
+        }
+
+        public static void MergeArray(int[] A, int[] B)
+        {
+            int posA = A.Length - 1;
+            int posB = B.Length - A.Length - 1;
+
+            for (int i = B.Length - 1; i >= 0; i--)
+            {
+                if (i != 0)
+                {
+                    if (A[posA] > B[posB])
+                    {
+                        B[i] = A[posA];
+                        posA--;
+
+                    }
+                    else
+                    {
+                        B[i] = B[posB];
+                        posB--;
+                    }
+
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(", ", B));
+        }
+    }
+}
